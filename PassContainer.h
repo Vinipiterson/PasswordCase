9be84b856fa0469b8acaba4bafa2c34d704da21e
Vinipiterson <119087868+Vinipiterson@users.noreply.github.com>
2023-07-12 20:23:55 -0300

    Initial commit

---
+++ PassContainer.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include <string>
+
+class PassContainer
+{
+public:
+    PassContainer(std::string NLink = "TuaMae@gmail.com", std::string NPassword = "Tua mae é minha") : Link(NLink), Password(NPassword) {}
+    
+private:
+    std::string Link{};
+    std::string Password{};
+
+public:
+    inline  std::string GetLink() const { return Link; }
+    inline std::string GetPassword() const { return Password; }
+
+    inline void SetLink(std::string NLink) { Link = NLink; }
+    inline void SetPassword(std::string NPassword) { Link = NPassword; }
+};
